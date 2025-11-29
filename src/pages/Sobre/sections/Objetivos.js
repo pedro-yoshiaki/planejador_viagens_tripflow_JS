40940b5efee8997cd49888cfb1b94cfa7267b176
Pedro Yoshiaki <pedro.yoshiaki@gmail.com>
2025-11-29 18:09:31 -0300

    Componente ObjetivoCard

--- src/pages/Sobre/sections/Objetivos.js
+++ src/pages/Sobre/sections/Objetivos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Objetivos.css';
-import { FaCheckCircle, FaBullseye} from 'react-icons/fa';
+import { FaCheckCircle, FaBullseye, FaMapMarkedAlt, FaCalendarCheck, FaClipboardList, FaMousePointer} from 'react-icons/fa';
+import ObjetivoCard from '../../../components/features/ObjetivoCard/ObjetivoCard';
 
 function Objetivos (){
     return (
@@ -8,22 +9,26 @@ function Objetivos (){
                 <div className="container">
                   <h2><FaBullseye /> Objetivos do Sistema</h2>
                   <div className="objetivos-grid">
-                    <div className="objetivo-card">
-                      <FaCheckCircle className="icon-check" />
-                      <p>Facilitar o planejamento de viagens</p>
-                    </div>
-                    <div className="objetivo-card">
-                      <FaCheckCircle className="icon-check" />
-                      <p>Evitar conflitos de datas automaticamente</p>
-                    </div>
-                    <div className="objetivo-card">
-                      <FaCheckCircle className="icon-check" />
-                      <p>Organizar atividades com detalhes completos</p>
-                    </div>
-                    <div className="objetivo-card">
-                      <FaCheckCircle className="icon-check" />
-                      <p>Oferecer uma interface clara e fácil de usar</p>
-                    </div>
+
+                    <ObjetivoCard 
+                        icon={<FaMapMarkedAlt />} 
+                        texto="Facilitar o planejamento de viagens" 
+                    />
+
+                    <ObjetivoCard 
+                        icon={<FaCalendarCheck />} 
+                        texto="Evitar conflitos de datas automaticamente" 
+                    />
+                            
+                    <ObjetivoCard 
+                        icon={<FaClipboardList />} 
+                        texto="Organizar atividades com detalhes completos" 
+                    />
+                            
+                    <ObjetivoCard 
+                        icon={<FaMousePointer />} 
+                        texto="Oferecer uma interface clara e fácil de usar" 
+                    />
                   </div>
                 </div>
               </section>
