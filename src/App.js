dba7b2930a6892fc95b267bda7a724cde73f064f
Pedro Yoshiaki <pedro.yoshiaki@gmail.com>
2025-12-01 19:18:23 -0300

    Revisao de Projeto 1 = Retirada de arquivos sobressalentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/layout/Header/Header'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
@@ -19,18 +17,3 @@ function App() {
 }
 
 export default App;
-
-/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
